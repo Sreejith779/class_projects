e705bc73c9f7f5f111019a974ae71465ee5c6eba
Sreejith779 <sreejithhkm@gmail.com>
2024-03-27 12:54:45 +0530

    initial

--- lib/features/splashPage/ui/splashScreen.dart
+++ lib/features/splashPage/ui/splashScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,16 +11,25 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    CircularProgressIndicator();
+
     Future.delayed(Duration(seconds: 10));
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Colors.yellowAccent,
+      backgroundColor: Colors.white,
       body: Center(
+        child: SizedBox(
+          height: 180,
+          child: Lottie.network("https://lottie.host/b71b0a42-085c-4dd5-8aa2-ced5e84ba563/nQS64QepIy.json"),
 
-    ));
+
+          // decoration: BoxDecoration(
+          //   image: DecorationImage(image: NetworkImage("https://www.creativefabrica.com/wp-content/uploads/2023/02/18/Fall-Gnome-61657046-1.png"))
+          // ),
+        )
+    )
+    );
   }
 }
